f54e04a82aa8a1eb36c73cd0b4a213ef17135852
youssef <youssef@gmail.com>
2025-07-31 12:10:44 +0300

    upload

--- src/modules/order/order.controller.js
+++ src/modules/order/order.controller.js
@@ -2,6 +2,7 @@ import cartModal from "../../../DB/model/cart.modal.js"
 import orderModal from "../../../DB/model/order.modal.js"
 import productModal from "../../../DB/model/product.modal.js"
 import AppError from "../../utils/appError.js";
+import Refactor from "../handler/reFactor.js";
 
 
 const cashOrder = async (req, res, next) => {
@@ -35,16 +36,13 @@ const getOrder = async (req, res, next) => {
     return res.status(200).json({ message: "success", order: order })
 }
 
-const getAllOrder = async (req, res, next) => {
-    const orders = await orderModal.find({})
-    return res.status(200).json({ message: "success", orders: orders })
-}
 const cancelOrder = async (req, res, next) => {
     const isCancel = await orderModal.findOneAndDelete({ user: req.user._id, _id: req.params.id })
-    if (!isCancel) return next(new Error('fail', { cause: 401 }))
+    if (!isCancel) return next(new AppError('fail', 401))
     return res.status(200).json({ message: 'success' })
 }
 
+const getAllOrder = new Refactor(orderModal).getAll()
 
 
 export {
